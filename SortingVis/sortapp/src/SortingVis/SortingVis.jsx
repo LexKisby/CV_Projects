2679b138fc8e11c4287b280b090c939f808f5149
LexKisby <alexander.r.kisby@durham.ac.uk>
2020-03-31 21:00:55 +0100

    hate css

--- SortingVis/sortapp/src/SortingVis/SortingVis.jsx
+++ SortingVis/sortapp/src/SortingVis/SortingVis.jsx
@@ -1,55 +1,63 @@
-import React from 'react';
-import './SortingVis.css';
+import React from "react";
+import "./SortingVis.css";
 
 export default class SortingVis extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          array: [],  
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: []
+    };
+  }
 
-    componentDidMount() {
-        this.resetArray(1);
-    }
+  componentDidMount() {
+    this.resetArray(1);
+  }
 
-    resetArray(contiguous) {
-        const array = [];
-        if (contiguous) {
-            for (let x = 1; x < 101; x++) {
-                array.push(x);
-            }
-            shuffle(array)
-        } else {
-            for (let x =1; x<101; x++) {
-                array.push(between(5, 1000))
-            }
-        }
-        this.setState({array});
+  resetArray(contiguous) {
+    const array = [];
+    if (contiguous) {
+      for (let x = 1; x < 101; x++) {
+        array.push(x * 8);
+      }
+      shuffle(array);
+    } else {
+      for (let x = 1; x < 101; x++) {
+        array.push(between(5, 1000));
+      }
     }
-    render() {
-        const {array} = this.state;
+    this.setState({ array });
+  }
+  render() {
+    const { array } = this.state;
 
-        return (
-            <>
-                {array.map((value, idx) => (
-                    <div className="array-bar" key={idx}>
-                        {value}
-                    </div>
-                ))}
-            </>
-        );
-    }
+    return (
+      <>
+        <div className="Control-bar">
+          <h3>Control Bar</h3>
+          <p>The controls are all here</p>
+        </div>
+        <div className="array-container">
+          {array.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{ height: `${value}px` }}
+            >
+              {value}
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 function between(min, max) {
-    return Math.floor(
-        Math.random() * (max-min) + min
-    )
+  return Math.floor(Math.random() * (max - min) + min);
 }
 function shuffle(array) {
-    for (let i = array.length -1; i>0; i--) {
-        let j = Math.floor(Math.random() * (i+1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
